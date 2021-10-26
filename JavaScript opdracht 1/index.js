b33d6be020f0986c6fe9194b439ad19020a687ee
tattooremco <89543896+tattooremco@users.noreply.github.com>
2021-10-26 21:57:36 +0200

    Add files via upload

---
+++ JavaScript opdracht 1/index.js
@@ -0,0 +1,17 @@
+console.log("Hello Winc Academy")
+let naam = 'Remco';
+console.log(naam)
+let sum = 4 + 4;
+console.log(sum)
+let plus = "4" + "4";
+console.log(plus)
+let min = 16 - 10;
+console.log(min)
+let maal = 5 * 5;
+console.log(maal)
+let delen = 50 / 5;
+console.log(delen)
+let age = 47;
+console.log(typeof age)
+let leeftijd = 'zevenenveertig';
+console.log(typeof leeftijd)
