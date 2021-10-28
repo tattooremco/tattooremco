5396b9229cd3862f0f193254ee00e4acaa6aec14
tattooremco <89543896+tattooremco@users.noreply.github.com>
2021-10-28 22:13:18 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const age = 19;
+if (age >= 18) {
+
+    console.log("Deze persoon mag naar binnen")
+
+} else {
+
+    console.log("Deze persoon is nog te jong en mag niet naar binnen")
+
+}
+
+const ifFemale = true;
+if (ifFemale) {
+
+    console.log("Dit is een vrouw dus ze mag kaartjes kopen")
+
+} else {
+
+    console.log("Dit is geen vrouw dus diegene mag geen kaartjes kopen")
+
+}
+
+const driverStatus = 'Bob';
+if (driverStatus == 'Bob') {
+
+    console.log("Deze Bob mag rijden!")
+
+} else {
+
+    console.log("Sorry maar deze persoon mag niet meer rijden")
+
+}
