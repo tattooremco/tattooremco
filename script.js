7d997d9a43a7e6da2c5200dbc331c6078f60e929
tattooremco <89543896+tattooremco@users.noreply.github.com>
2021-11-02 22:43:33 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const wallPaint = function (wall, colour) {
+
+    console.log("The " + wall, "wall has been painted " + colour);
+    console.log("");
+
+}
+
+wallPaint("north", "green");
+wallPaint("south", "white");
+wallPaint();
