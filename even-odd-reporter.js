c021e7ba83709f47b4bea227e957f19f76ba787e
tattooremco <89543896+tattooremco@users.noreply.github.com>
2021-11-10 22:37:08 +0100

    Add files via upload

---
+++ even-odd-reporter.js
@@ -0,0 +1,9 @@
+// For loop
+
+for (let i = 1; i <= 20; i++) {
+    console.log(`Is ${i} even or odd?`);
+    if (i % 2 == 0)
+        console.log(`${i} is even`);
+    else
+        console.log(`${i} is odd`);
+};
---
+++ multiplication-tables.js
@@ -0,0 +1,9 @@
+// For loop
+
+for (let number = 1; number <= 10; number++) {
+
+    for (let i = 1; i <= 10; i++) {
+        const result = i * number;
+        console.log(`${number} * ${i} = ${result}`);
+    }
+}
